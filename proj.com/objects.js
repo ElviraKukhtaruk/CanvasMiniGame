9c2a314eb8b5e7f6111d1d5625ee31afd5c62949
Josef Miegl <josef@miegl.cz>
2021-11-13 00:13:42 +0100

    improvements

--- proj.com/objects.js
+++ proj.com/objects.js
@@ -1,19 +1,28 @@
+function calculateX(width, box) {
+	return Math.floor((Math.random() * Math.floor((width-2*box)/box) + 1)) * box;
+}
+
+function calculateY(placeMin, placeMax) {
+	return Math.floor((Math.random() * (placeMax - placeMin) + placeMin));
+}
+
 let object = {
+	_objects: [],
 
-  _objects: [],
+	_setObjectConstructor(ctx, damage, money, placeMin, placeMax, image, width, box) {
+		let len = this._objects.push({
+			x: calculateX(width, box),
+			y: calculateY(placeMin, placeMax),
+			damage: damage,
+			money: money,
+			placeMin: placeMin,
+			placeMax: placeMax,
+			image: image
+		});
+		ctx.fillRect(this._objects[len-1].x, this._objects[len-1].y, 40, 30);
+	},
 
-  _setObjectConstructor(ctx, lengthFrom, lengthTo, placeMax, placeMin){
-          for(let i=lengthFrom; i<lengthTo; i++){
-            this._objects[i] = {
-              x: Math.floor((Math.random() * 59 + 1)) * 32,
-              y: Math.floor((Math.random() * (placeMax - placeMin) + placeMin))
-            };
-            ctx.fillRect(this._objects[i].x, this._objects[i].y, 40, 30);
-          } 
-     },
-
-    getObject(){
-       return this._objects;
-    }
-
+	getObject(){
+		return this._objects;
+	}
 }
